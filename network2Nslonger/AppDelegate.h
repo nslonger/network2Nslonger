00142d49833ffd82262a05cf715c7ddf0b93d733
Nslonger <nslonger@sina.com>
2016-04-22 12:32:08 +0800

    refresh myApp

---
+++ network2Nslonger/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  network2Nslonger
+//
+//  Created by iDoctor on 16/4/22.
+//  Copyright © 2016年 iDoctor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
